35c1323f3f882f0b45ed2b8a56bad883ffcd7cc8
barryjung <wjdgusrbs5@gmail.com>
2023-06-07 16:51:32 +0900

    :sparkles:Feat: 퀴즈 제안하기 기능 작성
    퀴즈 뷰에 post로 동작하는 퀴즈 제안하기 기능을 작성했습니다.

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from quizzes.serializers import QuizSerializer, OptionSerializer
 
-# Create your views here.
+
+class QuizView(APIView):
+    """퀴즈 뷰
+
+    post요청시 퀴즈를 제안 받습니다.
+    """
+
+    def post(self, request):
+        """퀴즈 뷰 post
+
+        퀴즈를 제안하는 요청을 받습니다.
+        """
+        quiz = QuizSerializer(data=request.data["quiz"])
+        if quiz.is_valid():
+            save_quiz = quiz.save()
+        else:
+            return Response(quiz.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        options = OptionSerializer(
+            data=request.data["options"], many=True, quiz_id=save_quiz.id
+        )
+        if options.is_valid():
+            options.save()
+        else:
+            return Response(options.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"message": "제출완료"}, status=status.HTTP_201_CREATED)
