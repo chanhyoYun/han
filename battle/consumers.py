56e8410ec15d06092496158856fadaf53e1bde0d
Churnobyl <tjdcjfals@gmail.com>
2023-06-15 17:17:59 +0900

    :bug:Fix: 웹소켓 액세스 토큰 정보 불러오기

--- battle/consumers.py
+++ battle/consumers.py
@@ -7,10 +7,7 @@ from asgiref.sync import sync_to_async
 from urllib.parse import parse_qs
 from jwt import decode as jwt_decode
 from channels.db import database_sync_to_async
-from django.contrib.sessions.models import Session
-from django.core.cache import cache
-from redis import Redis
-import base64
+import os
 
 
 class BattleConsumer(AsyncWebsocketConsumer):
@@ -18,10 +15,6 @@ class BattleConsumer(AsyncWebsocketConsumer):
 
     배틀 연결 및 연결 해제
     """
-
-    @database_sync_to_async
-    def get_session(self, session_key):
-        return Session.objects.get(session_key=session_key)
 
     async def connect(self):
         """웹소켓 연결
@@ -33,12 +26,12 @@ class BattleConsumer(AsyncWebsocketConsumer):
         self.user = self.scope["user"].username
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-        session_key = parse_qs(self.scope["query_string"].decode())["sessionKey"][0]
-        # session = await self.get_session(session_key)
-        r = Redis(host="localhost", port=6379, db=0)
-        encoded_data = r.get(session_key).decode()
-        # + "=" * (4 - len(r.get(session_key).decode()) % 4)
-        print(base64.b64decode(encoded_data))
+        self.access_token_data = jwt_decode(
+            self.scope["cookies"]["access_token"],
+            key=os.environ.get("SECRET_KEY"),
+            algorithms="HS256",
+        )
+        print(self.access_token_data)
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         # 웹소켓 연결 시점
