abcc180e39d090dfc05854e5d4be81cf4f066509
0v0baek <qorqor4592@gmail.com>
2023-06-19 20:56:45 +0900

    :hammer:Refact: 퀴즈 제네레이터 리팩토링
    23.06.19.
    쿼리문으로 유형별 퀴즈 제출 가능하게 뷰 수정

--- crawled_data/views.py
+++ crawled_data/views.py
@@ -25,6 +25,11 @@ from crawled_data.serializers import NaverQuizSerializer, KrDictQuizSerializer
 
 class PuzzleCreateView(APIView):
     def get(self, request):
-        quiz_generator = QuizGenerator([3, 3, 4, 0])
+        param = request.GET.get('type')
+        param_dict = {"1" : [10, 0, 0, 0], "2" : [0, 10, 0, 0], "3" : [0, 0, 10, 0]}
+        if param :
+            quiz_generator = QuizGenerator(param_dict[param])
+        else:
+            quiz_generator = QuizGenerator([3, 3, 4, 0])
         quiz = quiz_generator.generator()
         return Response(quiz)
