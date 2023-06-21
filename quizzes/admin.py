9bbac05ec2c2b3ad11e9074ca4f49e5bd38b4f63
0v0baek <qorqor4592@gmail.com>
2023-06-21 21:43:31 +0900

    :bug:Fix: 퀴즈 출제 뷰 수정 등
    23.06.21.
    십자말 형식 퀴즈도 출제 가능하게 변경
    users와 quizzes 어드민 일부 수정

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from quizzes.models import UserQuiz, UserQuizoption
+from quizzes.models import *
 
 admin.site.register(UserQuiz)
 admin.site.register(UserQuizoption)
+admin.site.register(QuizReport)
