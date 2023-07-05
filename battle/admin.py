31651357495b5f4356b39b6d4234a383a3789eb5
0v0baek <qorqor4592@gmail.com>
2023-07-05 20:54:14 +0900

    :ambulance:!HOTFIX: battle admin 오류
    23.07.05.
    battle app 하위의 admin.py에서 제대로 지워지지 않은 코드 때문에 실행되지
    않은 문제 해결

--- battle/admin.py
+++ battle/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from battle.models import *
 
 admin.site.register(BattleUser)
-admin.site.register(BattleHistory)
 admin.site.register(CurrentBattleList)
 admin.site.register(Notification)
